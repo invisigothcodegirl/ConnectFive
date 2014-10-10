824fa8607f5b1ab1e653addff74dd74a1442999d
invisigothcodegirl <jennifer.bailey@aims.edu>
2014-10-09 22:21:10 -0600

    Game logic is in place in checkWins to check for vertical and horizontal wins

--- ConnectFive/GameScene.swift
+++ ConnectFive/GameScene.swift
@@ -23,14 +23,15 @@ class GameScene: SKScene {
     var gridPoints : [[CGPoint]] = []
     var bottomRowSpheres : [Sphere?] = []
     var topSpheres : [[Sphere?]] = []
+    let GutterWidth : CGFloat = 3.0
+    let TopMargin : CGFloat = 50
   
     
     
     ///I'm adding a comment here *(&*&(&(&*(*&(&(*&(*&
     override func didMoveToView(view: SKView) {
-        let layerPosition =  CGPoint(
-            x:(size.width - (CGFloat(NumCols) * (TileWidth) + 3*(CGFloat(NumCols)-1)))/2,
-            y:size.height - CGFloat(NumRows) * (TileHeight + 3) - 20)
+        let layerPosition =  CGPoint(x:(size.width - (CGFloat(NumCols) * (TileWidth) + GutterWidth*(CGFloat(NumCols)-1)))/2,
+            y:size.height - CGFloat(NumRows) * (TileHeight + GutterWidth) - TopMargin)
         anchorPoint = CGPoint(x:0,y:0)
         tilesLayer.position = layerPosition
         spheresLayer.position = layerPosition
@@ -86,9 +87,91 @@ class GameScene: SKScene {
         }
     }
     
-    func checkWins(column : Int) {
-        println("Check for Wins!")
-        println(column)
+    func checkWins(row: Int, column : Int) {
+        var leftTot : Int = 0
+        var rightTot : Int = 0
+        var upTot : Int = 0
+        var downTot : Int = 0
+        //Check for horizontal wins
+        //count left
+        for var c = column; c > column - 4; c-- {
+            if c > 0 {
+                if let sphere1 = topSpheres[row][c]?.sphereType {
+                    if let sphere2 = topSpheres[row][c-1]?.sphereType {
+                        
+                        if sphere1 == sphere2 {
+                            leftTot++
+                            println("They match left")
+                        }
+                        else { break }
+                    }
+                    
+                }
+            }
+        }
+    
+        //count right
+        for var c = column; c < column + 4; c++ {
+            if c < NumCols - 1 {
+                if let sphere1 = topSpheres[row][c]?.sphereType {
+                    if let sphere2 = topSpheres[row][c+1]?.sphereType {
+                        
+                        if sphere1 == sphere2 {
+                            rightTot++
+                            println("They match right ")
+                        }
+                        else { break }
+                    }
+                    
+                }
+            }
+        }
+        
+        
+        if(leftTot + rightTot > 3) {
+            println("5 in a row!!!")
+        }
+        
+        //Check for vertical wins
+        //count up
+        for var r = row; r < row + 4; r++ {
+            if r < NumRows - 1 {
+                if let sphere1 = topSpheres[r][column]?.sphereType {
+                    if let sphere2 = topSpheres[r+1][column]?.sphereType {
+                        
+                        if sphere1 == sphere2 {
+                            upTot++
+                            println("They match up ")
+                        }
+                        else { break }
+                    }
+                    
+                }
+            }
+        }
+        //Probably wont need below
+        
+        //count down
+        /*for var r = row; r > row - 4; r-- {
+            if r > 0 {
+                if let sphere1 = topSpheres[r][column]?.sphereType {
+                    if let sphere2 = topSpheres[r-1][column]?.sphereType {
+                        
+                        if sphere1 == sphere2 {
+                            downTot++
+                            println("They match up ")
+                        }
+                        else { break }
+                    }
+                    
+                }
+            }
+        }*/
+        
+        if(upTot + downTot > 3) {
+            println("5 in a row!!!")
+        }
+        
     }
     
     func shuffleBottomSpheresLeft() {
@@ -134,7 +217,8 @@ class GameScene: SKScene {
     
 
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
+    {
         /* Called when a touch begins */
         
        
@@ -154,11 +238,12 @@ class GameScene: SKScene {
                     //Move to the top most row
                     
                     s.row = r
-                    s.sprite?.runAction(SKAction.sequence([move, SKAction.runBlock({self.checkWins(column)})]))
                     //Take out of bottom array
                     topSpheres[r][column] = s
                     bottomRowSpheres[column] = nil
                     shuffleRightFrom(column)
+                    s.sprite?.runAction(SKAction.sequence([move, SKAction.runBlock({self.checkWins(r, column: column)})]))
+                   
                 }
                 
                
@@ -185,9 +270,9 @@ class GameScene: SKScene {
     func getNextAvailableRowIn(column : Int) -> Int?
     {
         for var row = NumRows - 1; row > 0; row-- {
-            println("row and column")
-            println(row)
-            println(column)
+            //println("row and column")
+            //println(row)
+            //println(column)
             if let s = topSpheres[row][column]
             {
                 
